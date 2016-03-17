8ebe56cc566c81a338745d38748bb66e2c9fb362
Thomas Citharel <tcit@tcit.fr>
2016-03-17 14:34:00 +0100

    model

--- GestionMusique/core/models.py
+++ GestionMusique/core/models.py
@@ -3,3 +3,28 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+	name = models.CharField(max_length=200)
+
+class Singer(models.Model):
+	name = models.CharField(max_length=200)
+
+class Genre(models.Model):
+	label = models.CharField(max_length=200)
+	def __str__(self):
+		return self.label
+
+class Song(models.Model):
+	id = models.IntegerField(primary_key=True)
+	title = models.CharField(max_length=200)
+	author = models.OneToOneField(Author)
+	img = models.CharField(max_length=200)
+	releaseYear = models.IntegerField()
+	singer = models.OneToOneField(Singer)
+	genres = models.ManyToManyField(Genre, through="Classification")
+
+class Classification(models.Model):
+	song = models.ForeignKey(Song)
+	genre = models.ForeignKey(Genre)
+
