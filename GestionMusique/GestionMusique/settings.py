7643dad27df1c90dfc196353a81ae4a3382a95e9
Thomas Citharel <tcit@tcit.fr>
2016-03-17 14:22:28 +0100

    try

--- GestionMusique/GestionMusique/settings.py
+++ GestionMusique/GestionMusique/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crud',
 ]
 
 MIDDLEWARE_CLASSES = [
--- GestionMusique/core/apps.py
+++ GestionMusique/core/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class CrudConfig(AppConfig):
-    name = 'crud'
+class CoreConfig(AppConfig):
+    name = 'core'
