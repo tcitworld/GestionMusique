2b6d8d858a0f849d04906252b4b8df19e0b17cc7
Vincent <vincentbechu@gmail.com>
2016-03-22 09:05:05 +0100

    non fix

--- GestionMusique/core/management/commands/loadbdd.py
+++ GestionMusique/core/management/commands/loadbdd.py
@@ -2,7 +2,7 @@
 
 from django.core.management.base import BaseCommand, CommandError
 from core.models import *
-import yaml,sys,urllib
+import yaml,sys,urllib.parse
 
 class Command(BaseCommand):
 	help = "Load the database"
@@ -12,19 +12,23 @@ class Command(BaseCommand):
 
 	def handle(self,**option):
 		songs = yaml.load(open(option['filename'][0]))
-		tt = len(songs)
+		tt = 200
 		for i in range(1,tt):
 			s = songs[i]
-			if s['by'] != 'X':
-				a,isNew = Artist.objects.update_or_create( name=s['by'])
-				if isNew:
-					a.save()
+			if s['by'] == 'X' or s['by'] == None or s['by'] == 'null':
+				a,isNew = Artist.objects.update_or_create(name='inconnu')
 			else:
-				a = None
-			si,isNew = Group.objects.update_or_create( name=s['parent'])
+				a,isNew = Artist.objects.update_or_create( name=s['by'])
+			if isNew:
+				a.save()
+
+			if s['parent'] == None or s['parent'] == 'null':
+				si,isNew = Group.objects.update_or_create( name='inconnu')
+			else:
+				si,isNew = Group.objects.update_or_create( name=s['parent'])
 			if isNew:
 				si.save()
-			song,isNew = Song.objects.update_or_create(title=s['title'], artist=a, img=urllib.quote(s['img']) if s['img'] != None else None, releaseYear=s['releaseYear'], group=si)
+			song,isNew = Song.objects.update_or_create(title=s['title'], artist=a, img=urllib.parse.quote(s['img']) if s['img'] != None else None, releaseYear=s['releaseYear'], group=si)
 			if isNew:
 				song.save()
 			for genre in s['genre']:
