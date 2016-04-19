d9800a04e071c74647c7634d8bb8c68512b1f649
Vincent <vincentbechu@gmail.com>
2016-04-19 17:13:15 +0200

    change song to album

--- GestionMusique/player/urls.py
+++ GestionMusique/player/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home,name='url_home'),
-    url(r'^song/(?P<song_id>[0-9]+)/$', views.song),
+    url(r'^album/(?P<song_id>[0-9]+)/$', views.album),
     url(r'^author/(?P<artist_id>[0-9]+)/$', views.artist),
     url(r'^group/(?P<group_id>[0-9]+)/$', views.group),
     url(r'^login/$',views.loginUser,name='url_login'),
